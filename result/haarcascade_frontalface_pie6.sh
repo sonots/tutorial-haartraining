a286c27d6de5ee7810026296728c839687876187
sonots <sonots@f7d8f97f-4b4e-0410-be5b-f55f1808d820>
2008-09-18 00:14:57 +0000

    add test

--- result/haarcascade_frontalface_pie6.sh
+++ result/haarcascade_frontalface_pie6.sh
@@ -17,8 +17,10 @@
 #  -vec samples.vec -bg negatives.dat -nstages 20 -nsplits 2 -minhitrate 0.999 \
 #  -maxfalsealarm 0.5 -npos 10000 -nneg `cat negatives.dat | wc -l` \
 #  -w 18 -h 20 -mem 500 -mode ALL -maxtreesplits 4 > log &
+
 ./performance -data haarcascade_frontalface_pie6.xml -info pie_tests.dat -ni \
   |& tee haarcascade_frontalface_pie6.performance_pie_tests.txt
+# cp ../../result/cmu_tests.dat .
 ./performance -data haarcascade_frontalface_pie6.xml -info cmu_tests.dat -ni \
   |& tee haarcascade_frontalface_pie6.performance_cmu_tests.txt
 
